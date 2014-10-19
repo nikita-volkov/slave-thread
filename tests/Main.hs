de3db7084137c7674659bd0bb1bd0a2f6bfb5aab
Nikita Volkov <nikita.y.volkov@mail.ru>
2014-10-19 08:40:15 +0400

    Update tests

--- tests/Main.hs
+++ tests/Main.hs
@@ -5,6 +5,7 @@ import BasePrelude
 import Test.Framework
 import Test.QuickCheck.Instances
 import qualified SlaveThread as S
+import qualified Control.Concurrent.SSem as SSem
 
 
 main = 
@@ -15,55 +16,61 @@ test_forkedThreadsRunFine =
   replicateM 100 $ do
     var <- newIORef 0
     let increment = modifyIORef var (+1)
+    semaphore <- SSem.new 0
     S.fork $ do
       increment
+      semaphore' <- SSem.new (-1)
       S.fork $ do
         increment
+        SSem.signal semaphore'
       S.fork $ do
         increment
-      threadDelay $ 10^5
-    threadDelay $ 20 * 10^3
+        SSem.signal semaphore'
+      SSem.wait semaphore'
+      SSem.signal semaphore
+    SSem.wait semaphore
     assertEqual 3 =<< readIORef var
 
 test_killingAThreadKillsDeepSlaves = 
-  replicateM 1000 $ do
+  replicateM 100 $ do
     var <- newIORef 0
     let increment = modifyIORef var (+1)
+    semaphore <- SSem.new 0
     thread <- 
-      S.fork $ do
-        S.fork $ do
-          S.fork $ do
-            threadDelay $ 10^5
+      S.forkFinally (SSem.signal semaphore) $ do
+        w <- forkWait $ do
+          w <- forkWait $ do
+            threadDelay $ 20*10^3
             increment
-          S.fork $ do
-            increment
-          threadDelay $ 10^6
-        threadDelay $ 10^6
-    threadDelay $ 10^4
+          w
+        w
     killThread thread
-    assertEqual 1 =<< readIORef var
+    SSem.wait semaphore
+    assertEqual 0 =<< readIORef var
 
 test_dyingNormallyKillsSlaves = 
   replicateM 100 $ do
     var <- newIORef 0
     let increment = modifyIORef var (+1)
-    S.fork $ do
+    semaphore <- SSem.new 0
+    S.forkFinally (SSem.signal semaphore) $ do
       S.fork $ do
         threadDelay $ 10^4
         increment
       S.fork $ do
         threadDelay $ 10^4
         increment
-    threadDelay $ 10^5
+    SSem.wait semaphore
     assertEqual 0 =<< readIORef var
 
 test_finalizationOrder = 
   replicateM 1000 $ do
     var <- newIORef 0
-    S.forkFinally (modifyIORef var (*2)) $ do
+    semaphore <- SSem.new 0
+    S.forkFinally (modifyIORef var (*2) >> SSem.signal semaphore) $ do
       S.forkFinally (modifyIORef var (+1)) $ return ()
       S.forkFinally (modifyIORef var (+1)) $ return ()
-    threadDelay $ 10^4
+    SSem.wait semaphore
     assertEqual 4 =<< readIORef var
 
 test_exceptionsDontGetLost = 
@@ -75,3 +82,12 @@ test_exceptionsDontGetLost =
         threadDelay $ 10^4
       threadDelay $ 10^4
 
+forkWait :: IO a -> IO (IO ())
+forkWait io =
+  do
+    v <- newEmptyMVar
+    S.fork $ do
+      r <- try io
+      putMVar v ()
+      either (throwIO :: SomeException -> IO a) return r
+    return $ takeMVar v
